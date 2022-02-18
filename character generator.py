e3b7c8ee9fc77675ba9bdfa28899524b9834831d
NacreousDawn596 <75474839+NacreousDawn596@users.noreply.github.com>
2022-02-18 14:50:38 +0100

    Update character generator.py

--- character generator.py
+++ character generator.py
@@ -2,7 +2,7 @@ import itertools as its
 digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz²&é'(-è_çà)=~1234567890°+ù*%µ!:;,?./§<>¹~#{[|`^@]}¤ń”“¢»ł@ßðđŋħłµþø→↓←ŧ¶€«æ "
 r = its.product(digits, repeat=int(input("how many digits? \n->") ))
 name = f"passwords"
-dic = open (name, "w")
+dic = open(name, "w")
 for i in r:
     dic.write("".join(i))
     dic.write("".join("\n"))
